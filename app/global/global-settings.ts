60bb1a65461791904707b9cd8914532fc7190c24
Caleb Banzhaf <caleb.banzhaf@sntmedia.com>
2016-08-10 10:34:49 -0500

    some changes to header in core

--- app/global/global-settings.ts
+++ app/global/global-settings.ts
@@ -30,7 +30,8 @@ export class GlobalSettings {
     private static _siteFacebookUrl:string = 'https://www.facebook.com/touchdownloyal';
 
     private static _baseTitle: string = "Touchdown Loyal";
-    private static _basePartnerTitle: string = "My Touchdown Zone" //todo;
+    private static _basePartnerTitle: string = "My Touchdown Zone";
+    private static _sportName: string ="football";
 
     private static _sportLeagueAbbrv: string ="NFL";
     private static _sportLeagueFull: string ="National Football League";
@@ -232,6 +233,9 @@ export class GlobalSettings {
     }
     static getSiteFacebookUrl() {
       return this._siteFacebookUrl;
+    }
+    static getSportName() {
+      return this._sportName;
     }
 
 }
