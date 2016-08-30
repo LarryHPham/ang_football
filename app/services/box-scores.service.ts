4cf230b304ebad089d1f14d57be149d77ec527c6
larryhpham <larrypham1991@yahoo.com>
2016-08-30 09:19:42 -0500

    remove console

--- app/services/box-scores.service.ts
+++ app/services/box-scores.service.ts
@@ -172,7 +172,6 @@ export class BoxScoresService {
   }
 
   var callURL = this._apiUrl+'/'+profile+'/gameDates/'+teamId+'/'+ date;//localToEST needs tobe the date coming in AS UNIX
-  console.log(callURL);
   return this.http.get(callURL, {headers: headers})
     .map(res => res.json())
     .map(data => {
