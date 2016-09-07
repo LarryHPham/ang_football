63bc4a97e057d81612c015564c3ce0dd8b867994
Larry Pham <LarryHPham@users.noreply.github.com>
2016-09-07 16:20:27 -0500

    Update standings.page.ts
    s

--- app/webpages/standings-page/standings.page.ts
+++ app/webpages/standings-page/standings.page.ts
@@ -135,7 +135,6 @@ export class StandingsPage{
     if ( this.pageParams.teamId ) {
       this._profileService.getTeamProfile(this.pageParams.teamId).subscribe(
         data => {
-          console.log(data);
           this.profileLoaded = true;
           this.pageParams = data.pageParams;
           this.pageParams.scope = this.scope;
