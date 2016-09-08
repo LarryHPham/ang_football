faabad70eddd76551a4cead19f2726c353313123
coreywynn <Corey.Wynn@sntmedia.com>
2016-09-07 22:53:00 -0500

    Agile review edits for transactions and mvp

--- app/services/transactions.service.ts
+++ app/services/transactions.service.ts
@@ -13,6 +13,7 @@ import {TransactionsListInput} from '../fe-core/components/transactions-list-ite
 declare var moment: any;
 
 interface TransactionInfo {
+    affiliation: string;
     transactionDate: string;
     transactionType?: string;
     playerPosition: string;
@@ -223,6 +224,7 @@ export class TransactionsService {
         var teamRoute = VerticalGlobalFunctions.formatTeamRoute(val.teamName, val.teamId);
         var playerFullName = val.playerFirstName + ' ' + val.playerLastName;
         var playerRoute = null;
+        var scope = val.affiliation.toUpperCase();
 
         if (val.playerActive) {
           playerRoute = VerticalGlobalFunctions.formatPlayerRoute(val.teamName, playerFullName, val.playerId);
@@ -253,7 +255,7 @@ export class TransactionsService {
         return SliderCarousel.convertToCarouselItemType1(index, {
           backgroundImage: VerticalGlobalFunctions.getBackroundImageUrlWithStockFallback(val.backgroundImage),
           copyrightInfo: GlobalSettings.getCopyrightInfo(),
-          subheader: [tab.tabDisplay + ' - ', teamLinkText],
+          subheader: [tab.tabDisplay + ' - ', scope],
           profileNameLink: playerLinkText,
           description: [
               description
@@ -267,7 +269,6 @@ export class TransactionsService {
           // subImageRoute: teamRoute
         });
       });
-
     }
     return carouselArray;
   }
@@ -284,6 +285,19 @@ export class TransactionsService {
       var playerRoute = null;
       var playerFullName = val.playerFirstName + ' ' + val.playerLastName;
 
+      //Description conditional need updated when correct API gets set up and "Type" is added to JSON object
+      var description;
+
+      if (val.transactionType == "suspension") {
+        description = playerFullName + " " + val.playerPosition + " for the " + val.teamName +  " was " + val.contents;
+      }
+      else if (val.transactionType == "injuries") {
+        description = playerFullName + " " + val.playerPosition + " for the " + val.teamName +  " is out with " + val.contents;
+      }
+      else {
+        description = playerFullName + " was " + val.contents;
+      }
+
       if (val.playerActive) {
         playerRoute = VerticalGlobalFunctions.formatPlayerRoute(val.teamName, playerFullName, val.playerId);
       }
@@ -299,7 +313,7 @@ export class TransactionsService {
           style   : 'transactions-small',
           data_shortFormDate :   moment(val.transactionDate).format("MM/DD/YY"),
           data_longFormDate : moment(val.transactionDate).format("MMM. DD, YYYY"),
-          value   : [playerTextLink, val.contents],
+          value   : [description],
           url     : null
         }],
         imageConfig: TransactionsService.getListImageData(GlobalSettings.getImageUrl(val.playerImage), playerRoute)
