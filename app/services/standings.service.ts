9174145fa0e70e7bc2f407f8d6431a46c2fcd550
Caleb Banzhaf <caleb.banzhaf@sntmedia.com>
2016-09-09 09:43:15 -0500

    fixed standings module limit to 5 on league page

--- app/services/standings.service.ts
+++ app/services/standings.service.ts
@@ -219,13 +219,14 @@ export class StandingsService {
         for ( var conferenceKey in apiData ) {
           var divData: any = [];
           for ( var divisionKey in apiData[conferenceKey] ) {
-            for (var i = 0; i < apiData[conferenceKey][divisionKey].length; i++) {
+            for (var i = 0; i < apiData[conferenceKey][divisionKey].length && i < maxRows; i++) {
               divData.push(apiData[conferenceKey][divisionKey][i]);
+              totalRows++;
             }
             // totalRows += table.tableData.rows.length;
-            // if ( maxRows && totalRows > maxRows ) {
-            //   break; //don't add more divisions
-            // }
+            if ( maxRows && totalRows > maxRows ) {
+              break; //don't add more divisions
+            }
           }
           var table = this.setupTableData(standingsTab.currentTeamId, scope, conferenceKey, "Conference", divData, maxRows, true);
           sections.push(table);
