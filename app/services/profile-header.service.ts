59479bb4adfc78ae3bf3ae50053dfc5b75a58148
coreywynn <Corey.Wynn@sntmedia.com>
2016-09-12 13:05:10 -0500

    Update profile-header.service.ts

--- app/services/profile-header.service.ts
+++ app/services/profile-header.service.ts
@@ -188,8 +188,6 @@ export class ProfileHeaderService {
     let url = GlobalSettings.getApiUrl();
     url = url + '/profileHeader/team/' + teamId;
 
-    console.log(url);
-
     return this.http.get(url)
         .map(res => res.json())
         .map(data => {
