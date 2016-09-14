ff18b8d1feea29e240b392d3034a1d186bfe3be6
Navyaeetaram <navya.eetaram@sntmedia.com>
2016-09-14 13:35:28 -0500

    changes

--- app/webpages/syndicated-article-page/syndicated-article-page.page.ts
+++ app/webpages/syndicated-article-page/syndicated-article-page.page.ts
@@ -130,6 +130,7 @@ export class SyndicatedArticlePage{
                   this.getRecomendationData();
                 }
             );
+        console.log(this.geoLocation);
     }
 
     getPartnerHeader(){//Since it we are receiving
@@ -150,9 +151,11 @@ export class SyndicatedArticlePage{
     }
 
     getRecomendationData(){
+        console.log(this.geoLocation, "----------->");
       var startNum=Math.floor((Math.random() * 49) + 1);
         var state = 'KS'; //needed to uppoercase for ai to grab data correctly
         this._deepdiveservice.getRecArticleData(this.scope, this.geoLocation,startNum, 3)
+
           .subscribe(data => {
             this.recomendationData = this._deepdiveservice.transformToRecArticles(data);
              
