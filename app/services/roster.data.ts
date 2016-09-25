c717babf6413998d32552cf969800c92e83e444a
Jonathan Van Winkle <jonathan.vanwinkle@sntmedia.com>
2016-09-24 21:22:39 -0500

    Fixing Roster Carousel text:  https://www.wrike.com/open.htm?id=110839298

--- app/services/roster.data.ts
+++ app/services/roster.data.ts
@@ -138,8 +138,8 @@ export class NFLRosterTabData implements RosterTabData<TeamRosterData> {
       formattedSalary = "$" + GlobalFunctions.nFormatter(Number(val.playerSalary));
     }
 
-    var playerNum = val.playerJerseyNumber != null ? "<span class='text-heavy'>No. " + val.playerJerseyNumber + "</span>," : "";
-    var playerHeight = val.playerHeight != null ? "<span class='text-heavy'>" + VerticalGlobalFunctions.formatHeightInches(val.playerHeight) + "</span>, " : "";
+    var playerNum = val.playerJerseyNumber != null ? "<span class='text-heavy'>No. " + val.playerJerseyNumber + "</span>" : "";
+    var playerHeight = val.playerHeight != null ? "<span class='text-heavy'>" + VerticalGlobalFunctions.formatHeightInches(val.playerHeight) + "</span> " : "";
     var playerWeight = val.playerWeight != null ? "<span class='text-heavy'>" + val.playerWeight + "</span> " : "N/A";
     var playerSalary = "<span class='text-heavy'>" + formattedSalary + "</span> per year.";
 
@@ -150,7 +150,7 @@ export class NFLRosterTabData implements RosterTabData<TeamRosterData> {
     }
     var teamLinkText = {
       route: teamRoute,
-      text: val.teamName,
+      text: val.teamName + ",",
       class: 'text-heavy'
 
     }
@@ -180,11 +180,17 @@ export class NFLRosterTabData implements RosterTabData<TeamRosterData> {
       copyrightInfo: GlobalSettings.getCopyrightInfo(),
       subheader: [curYear + ' TEAM ROSTER'],
       profileNameLink: playerLinkText,
+      // NFL //
+      // [Player Name], [Position] for the [Team Name], is No. [##] and stands at [#’#”] tall, weighing [###]lbs. and making a salary of $[##,###]
+
+      // NCAAF //
+      // [Player Name], a [Class Year] [Position] for the [Team Name], is No. [##] and stands at [#’#”] tall, weighing [###]lbs.
+
       description: [
           playerLinkText,
           ", ",a,"<span class='text-heavy'>"+ classyear + val.playerPosition, "</span>",'for the ',
           teamLinkText,
-          'is <span class="text-heavy">'+ playerNum + '</span> and stands at ' + playerHeight + "tall, weighing " + playerWeight +"<span class='nfl-only'> and making a salary of "+ playerSalary + "</span>"
+          'is <span class="text-heavy">'+ playerNum + '</span> and stands at ' + playerHeight + "tall, weighing " + playerWeight +" lbs.<span class='nfl-only'> and making a salary of "+ playerSalary + "</span>"
       ],
       lastUpdatedDate: GlobalFunctions.formatUpdatedDate(val.lastUpdated),
       circleImageUrl: GlobalSettings.getImageUrl(val.playerHeadshotUrl),
