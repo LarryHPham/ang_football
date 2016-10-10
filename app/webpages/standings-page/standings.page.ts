cd2db3105e8071377fa4962386c6e02efb299d65
larryhpham <larrypham1991@yahoo.com>
2016-10-10 18:11:08 -0500

    remove console

--- app/webpages/standings-page/standings.page.ts
+++ app/webpages/standings-page/standings.page.ts
@@ -170,7 +170,6 @@ export class StandingsPage{
         text3: title,
         icon: "fa fa-map-marker"
       };
-      console.log();
       //for Seo purposes
       this.profileData = {
         headerData:{
