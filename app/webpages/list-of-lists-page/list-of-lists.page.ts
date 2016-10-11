90933aefc65a2c7121ab4246d3d2862e09befc0a
victoriale <le.victoriavan@gmail.com>
2016-10-11 10:29:13 -0500

    fix image issue for title on list of list player page

--- app/webpages/list-of-lists-page/list-of-lists.page.ts
+++ app/webpages/list-of-lists-page/list-of-lists.page.ts
@@ -98,19 +98,17 @@ export class ListOfListsPage implements OnInit{
                   listTargetData = list.targetData;
                 }
 
-
                 switch ( urlParams.target ) {
                     case "player":
                         profileName = listTargetData.playerFirstName + " " + listTargetData.playerLastName;
                         profileRoute = VerticalGlobalFunctions.formatPlayerRoute(listTargetData.teamName, profileName, listTargetData.playerId);
-                        profileImage = GlobalSettings.getImageUrl(listTargetData.imageUrl);
+                        profileImage = GlobalSettings.getImageUrl(listTargetData.playerHeadshotUrl);
                         break;
 
                     case "team":
                         profileName = listTargetData.teamName;
                         profileRoute = VerticalGlobalFunctions.formatTeamRoute(listTargetData.teamName, listTargetData.teamId);
                         profileImage = GlobalSettings.getImageUrl(listTargetData.teamLogo);
-
                         break;
 
                     default: break;
