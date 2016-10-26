a19a8610092f9cb27a9b94d0b176b98a9637d7f3
ricksellers <rick@ricksellers.design>
2016-10-26 15:50:08 -0500

    Fix for schedules stadium name, game date: https://www.wrike.com/open.htm?id=115206581 & https://www.wrike.com/open.htm?id=116115446

--- app/services/schedules.data.ts
+++ app/services/schedules.data.ts
@@ -150,11 +150,13 @@ export class SchedulesTableData implements TableComponentData<SchedulesData> {
       }
     }
 
+    item.venueStadium = item.venueStadium.split("(")[0];
+
     let displayedCarousel = {
       index:index,
       displayNext: displayNext,
       backgroundGradient: Gradient.getGradientStyles(colors),
-      displayTime: moment(Number(item.eventTimestamp)*1000).tz('America/New_York').format('dddd, MMM. Do, YYYY | h:mm A (z)'), //hard coded TIMEZOME since it is coming back from api this way
+      displayTime: moment(Number(item.eventTimestamp)*1000).tz('America/New_York').format('dddd, MMM. DD, YYYY | h:mm A (z)'), //hard coded TIMEZOME since it is coming back from api this way
       detail1Data:'Home Stadium:',
       detail1Value:item.venueStadium,
       detail2Value:stadiumLocation,
