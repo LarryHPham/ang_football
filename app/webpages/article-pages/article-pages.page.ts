d258e2952753b954d490e15306bbe168d332c3fe
lynchtroop5 <christopher.lynch@sntmedia.com>
2016-11-11 15:05:10 -0600

    Removal of check for scope in regards to ncaaf and fbs.

--- app/webpages/article-pages/article-pages.page.ts
+++ app/webpages/article-pages/article-pages.page.ts
@@ -114,8 +114,7 @@ export class ArticlePages implements OnInit {
                     this.getPartnerHeader();
                 }
                 if (this.eventType != 'story' && this.eventType != 'video') {
-                    //Temporary until fbs push by backend has been implemented
-                    this.scope = parentParams.scope == "nfl" ? "nfl" : "ncaaf";
+                    this.scope = parentParams.scope;
                     this.eventType = GlobalFunctions.getApiArticleType(_params.get('eventType'));
                     if (this.eventType == "articleType=player-fantasy") {
                         this.isFantasyReport = true;
