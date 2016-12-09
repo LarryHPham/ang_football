76f8b9eeadeff4031643334dbdb79a6db069bb73
coreywynn <Corey.Wynn@sntmedia.com>
2016-12-09 12:41:11 -0600

    pagination on mvp page and css tweaks

--- app/webpages/list-page/list.page.ts
+++ app/webpages/list-page/list.page.ts
@@ -127,6 +127,7 @@ export class ListPage {
 
 
     setDynamicPagination(input) {
+      console.log('setDynmaicPagination');
         var navigationParams = {
             query: this.query
         };
