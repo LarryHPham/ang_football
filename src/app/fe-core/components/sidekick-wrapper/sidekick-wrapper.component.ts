693c75c4a259c248afa5671b4f253a1e98a4c9ee
larryhpham <larrypham1991@yahoo.com>
2016-12-22 12:08:24 -0600

    connected submodule to core

--- src/app/fe-core/components/sidekick-wrapper/sidekick-wrapper.component.ts
+++
@@ -1,9 +0,0 @@
-
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'sidekick-wrapper',
-    templateUrl: './app/fe-core/components/sidekick-wrapper/sidekick-wrapper.component.html'
-})
-
-export class SidekickWrapper {}
