9481189cc1b56ffb57a5d21054f6086ef33e82ce
larryhpham <larrypham1991@yahoo.com>
2016-12-29 13:15:12 -0600

    update core

--- src/app/ui-modules/widget/widget.module.ts
+++ src/app/ui-modules/widget/widget.module.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {Http, Headers} from '@angular/http';
+import { isBrowser } from 'angular2-universal';
 declare var jQuery:any;
 
 @Component({
@@ -16,37 +17,39 @@ export class WidgetModule {
     isProSport:boolean = true;
 
     ngOnInit() {
-
+      if(isBrowser){
         this.isProSport = this.scope == 'nfl' || this.scope == 'home' ? true: false;
         var titleHeight = jQuery('.articles-page-title').height();
         var padding = document.getElementById('pageHeader').offsetHeight;
 
         if( document.getElementById('partner') != null){
-            var partnerHeight = document.getElementById('partner').offsetHeight;
-            padding += partnerHeight;
+          var partnerHeight = document.getElementById('partner').offsetHeight;
+          padding += partnerHeight;
         }
         if (!this.syndicated && !this.aiSidekick) {
-            this.headerHeight = padding + 'px';
+          this.headerHeight = padding + 'px';
         } else {
-            if (titleHeight == 40) {
-                this.headerHeight = padding + 95 + 'px';
-            } else if (titleHeight == 80) {
-                this.headerHeight = padding + 135 + 'px';
-            }
-            else if (titleHeight == 70) {
-              this.headerHeight = padding + 128 + 'px';
+          if (titleHeight == 40) {
+            this.headerHeight = padding + 95 + 'px';
+          } else if (titleHeight == 80) {
+            this.headerHeight = padding + 135 + 'px';
+          }
+          else if (titleHeight == 70) {
+            this.headerHeight = padding + 128 + 'px';
           }
           else if (titleHeight == 35) {
             this.headerHeight = padding + 102 + 'px';
+          }
         }
-        }
+      }
     }
 
     // Page is being scrolled
     onScroll(event) {
+      if(isBrowser){
         var partnerHeight = 0;
         if( document.getElementById('partner') != null){
-            partnerHeight = document.getElementById('partner').offsetHeight;
+          partnerHeight = document.getElementById('partner').offsetHeight;
         }
         var titleHeight = 0;
         var pageHeader = document.getElementById('pageHeader').offsetHeight;
@@ -57,30 +60,30 @@ export class WidgetModule {
         var maxScroll = (partnerHeight + parentElem) - scrollTop;
         var widgetTop = document.getElementById('widget').getBoundingClientRect().top;
         if (!this.syndicated && !this.aiSidekick) {
-            this.sidekickHeight = 0;
+          this.sidekickHeight = 0;
         } else {
           titleHeight = jQuery('.articles-page-title').height();
-            if (titleHeight == 40) {//
-                this.sidekickHeight = 95;
-            } else if (titleHeight == 80) {
-                this.sidekickHeight = 135;
+          if (titleHeight == 40) {//
+            this.sidekickHeight = 95;
+          } else if (titleHeight == 80) {
+            this.sidekickHeight = 135;
+          }
+          else if (titleHeight == 70) {
+            this.sidekickHeight = 128;
+          }
+          else if (titleHeight == 35) {
+            this.sidekickHeight = 102;
+          }
+          if (maxScroll <= 0) {
+            this.sidekickHeight += maxScroll;
+            if (this.sidekickHeight < 0) {
+              this.sidekickHeight = 0
             }
-            else if (titleHeight == 70) {
-                this.sidekickHeight = 128;
-            }
-            else if (titleHeight == 35) {
-                this.sidekickHeight = 102;
-            }
-            if (maxScroll <= 0) {
-                this.sidekickHeight += maxScroll;
-                if (this.sidekickHeight < 0) {
-                    this.sidekickHeight = 0
-                }
-            }
-            y_buffer += this.sidekickHeight;
+          }
+          y_buffer += this.sidekickHeight;
         }
         if(maxScroll <= 0){
-            maxScroll = 0;
+          maxScroll = 0;
         }
         //Logic below is to determine when to add or remove the class that makes the widget on the side Stick to the top using class with fixed position
         if ((document.getElementById('partner') != null && maxScroll == 0 && (parentElem - padding) < 10) || (document.getElementById('partner') == null && widgetTop <= padding && (parentElem - padding) < 10)) {
@@ -99,23 +102,24 @@ export class WidgetModule {
         var $widget = jQuery("#widget");
         var $pageWrapper = jQuery(".widget-page-wrapper");
         if ($widget.length > 0 && $pageWrapper.length > 0) {
-            var widgetHeight = $widget.height();
-            var pageWrapperTop = $pageWrapper.offset().top;
-            var pageWrapperBottom = pageWrapperTop + $pageWrapper.height() - padding;
-            if ((scrollTop + widgetHeight + y_buffer) > (pageWrapperBottom + this.sidekickHeight)) {
-                this.headerHeight = this.sidekickHeight + 'px';
-                $widget.addClass("widget-bottom");
-                var diff = $pageWrapper.height() - (widgetHeight + y_buffer);
-                $widget.get(0).style.top = diff + "px";
-            }
-            else if ((scrollTop + carContainer) < (pageWrapperTop + this.sidekickHeight)) {
-                $widget.removeClass("widget-bottom");
-                $widget.get(0).style.top = "";
-            }
-            else {
-                $widget.removeClass("widget-bottom");
-                $widget.get(0).style.top = "";
-            }
+          var widgetHeight = $widget.height();
+          var pageWrapperTop = $pageWrapper.offset().top;
+          var pageWrapperBottom = pageWrapperTop + $pageWrapper.height() - padding;
+          if ((scrollTop + widgetHeight + y_buffer) > (pageWrapperBottom + this.sidekickHeight)) {
+            this.headerHeight = this.sidekickHeight + 'px';
+            $widget.addClass("widget-bottom");
+            var diff = $pageWrapper.height() - (widgetHeight + y_buffer);
+            $widget.get(0).style.top = diff + "px";
+          }
+          else if ((scrollTop + carContainer) < (pageWrapperTop + this.sidekickHeight)) {
+            $widget.removeClass("widget-bottom");
+            $widget.get(0).style.top = "";
+          }
+          else {
+            $widget.removeClass("widget-bottom");
+            $widget.get(0).style.top = "";
+          }
         }
+      }
     }
 }
