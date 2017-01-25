da30bcd72007f5a179418ae6b7cdc63c842c0325
lynchtroop5 <christopher.lynch@sntmedia.com>
2017-01-25 17:33:20 -0600

    Merge remote-tracking branch 'origin/angular2_universal' into feature-lynch (reverted from commit 651a2a8304c23bf519daa03b3d0419c1f5748de2)

--- src/app/webpages/pickateam-page/pickateam.page.ts
+++ src/app/webpages/pickateam-page/pickateam.page.ts
@@ -1,6 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {isBrowser} from 'angular2-universal';
+import { ActivatedRoute } from '@angular/router';
 
 //globals
 import {GlobalSettings} from "../../global/global-settings";
@@ -107,13 +106,13 @@ export class PickTeamPage{
           this.setLocationHeaderString(res.state);
           this.getData(this.scope, res.state);
           //set the active league variables based on scope
-          if ( this.scope == this._collegeDivisonFullAbbrv.toLowerCase() ) {
+          if ( this.scope == this._collegeDivisionAbbrv.toLowerCase() ) {
             this.activeDivision = this._collegeDivisonFullAbbrv;
-            this.activeDivisionSegments = this._sportLeagueSegments;
+            this.activeDivisionSegments = this._collegeDivisionSegments;
           }
           else {
             this.activeDivision = this._sportLeagueAbbrv;
-            this.activeDivisionSegments = this._collegeDivisionSegments;
+            this.activeDivisionSegments = this._sportLeagueSegments;
           }
 
           this.homeHeading1 = "Stay Loyal to Your Favorite " + this.activeDivision + " Team";
@@ -151,16 +150,12 @@ export class PickTeamPage{
       this._seoService.setOgImage(image);
     } //metaTags
 
-
-
     setLocationHeaderString(state) {
       //only set for NCAAF
-      if ( this.scope.toLowerCase() == this._collegeDivisonFullAbbrv.toLowerCase() ) {
-        this.homeLocationHeading = 'Showing '+this._collegeDivisonFullAbbrv+ ' Football '+ this._collegeDivisionSegments + '<span class="location-designation"> located around <i ng-reflect-class-name="fa fa-map-marker" class="fa fa-map-marker"></i> ' + '<span class="location-title">' + GlobalFunctions.fullstate(state) + '</span>';
+      if ( this.scope == this._collegeDivisionAbbrv.toLowerCase() ) {
+        this.homeLocationHeading = 'Showing '+this.activeDivision+ ' Football '+ this.activeDivisionSegments.toLowerCase() + '<span class="location-designation"> located around <i ng-reflect-class-name="fa fa-map-marker" class="fa fa-map-marker"></i> ' + '<span class="location-title">' + GlobalFunctions.fullstate(state) + '</span>';
       }
-    } //setLocationHeaderString
-
-
+    }
 
     getData(scope, geoLocation?){
       this._pickateamPageService.getLandingPageService(scope, geoLocation)
@@ -171,9 +166,8 @@ export class PickTeamPage{
       var sampleImage = "./app/public/placeholder-location.jpg";
     } //getData
 
-
-
     ngOnDestroy(){
       this._routeSubscription.unsubscribe();
     }
+
 }
