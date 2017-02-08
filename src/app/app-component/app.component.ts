f733bed5ca636456cd7cba22ba902f6ef0aa583a
larryhpham <larrypham1991@yahoo.com>
2017-02-08 15:29:18 -0600

    test params

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -20,34 +20,35 @@ export class AppComponent {
     private _activatedRoute:ActivatedRoute,
     private _geoLocation: GeoLocation
   ) {
-      this._activatedRoute.firstChild.params.subscribe(
-        (params:any) => {
-          console.log(params);
-          this.scopeParam = '/'+params.scope;
-        }
-      )
       this._activatedRoute.params.subscribe(
         (params:any) => {
-          //function that grabs the designated location needed for the client and if a partnerID is sent through then it will also set the partnerID and partnerScript for their Header
-          if(GlobalSettings.getHomeInfo().isSubdomainPartner) {
-            if (isBrowser) {
-              var hostname = window.location.hostname;
-              this.partnerID = window.location.hostname.split('.')[1] + '.' + window.location.hostname.split('.')[2];
-            }
-          } else {
-            GlobalSettings.storedPartnerId(params.partnerID);
-            this.partnerID = params.partnerID;
-          }
-          this._geoLocation.grabLocation(this.partnerID).subscribe(res => {
-            if(res.partner_id){
-              GlobalSettings.storedPartnerId(res.partner_id);
-              this.partnerID = res.partner_id;
-            }
-            if(res.partner_script){
-              this.partnerScript = res.partner_script;
-            }
-          });
-        }
+          console.log(params);
+          this._activatedRoute.firstChild.params.subscribe(
+            (childParams:any) => {
+              console.log(childParams);
+              this.scopeParam = '/'+childParams.scope;
+              //function that grabs the designated location needed for the client and if a partnerID is sent through then it will also set the partnerID and partnerScript for their Header
+              if(GlobalSettings.getHomeInfo().isSubdomainPartner) {
+                if (isBrowser) {
+                  var hostname = window.location.hostname;
+                  this.partnerID = window.location.hostname.split('.')[1] + '.' + window.location.hostname.split('.')[2];
+                }
+              } else {
+                GlobalSettings.storedPartnerId(params.partnerID);
+                this.partnerID = params.partnerID;
+              }
+              this._geoLocation.grabLocation(this.partnerID).subscribe(res => {
+                if(res.partner_id){
+                  GlobalSettings.storedPartnerId(res.partner_id);
+                  this.partnerID = res.partner_id;
+                }
+                if(res.partner_script){
+                  this.partnerScript = res.partner_script;
+                }
+              });// end of geo location subscribe
+            }//end of child params
+          )
+        }//end of parent activated param
       );
   } //constructor
 
