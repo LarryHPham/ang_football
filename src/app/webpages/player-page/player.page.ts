acdace0ae1d5897753d26418c7193bcad8c0252b
larryhpham <larrypham1991@yahoo.com>
2017-02-13 13:24:58 -0600

    fixed team roster on player and team page from keeping old data when changing routes but keeping same page component view

--- src/app/webpages/player-page/player.page.ts
+++ src/app/webpages/player-page/player.page.ts
@@ -153,6 +153,7 @@ export class PlayerPage{
   //// This function contains values that need to be manually reset when navigatiing from player page to player page
   routeChangeResets() {
     this.isLoaded = false;
+    this.standingsData = null;
     this.batchLoadIndex = 1;
     if(isBrowser){
       window.scrollTo(0, 0);
